0683a2edb997f852cfd88e9242bbd3b6bb35590c
Jason_Smith <qili00001@gmail.com>
2021-06-15 21:10:19 +0800

    添加变量的使用

--- demo/01_helloPython.py
+++ demo/01_helloPython.py
@@ -1,2 +1,23 @@
-print("Hello Python")
-print("Hello myBug")
+# 保存每个程序时，使用符合标准Python约定的文件名：使用小写字母和下划线，如simple_message.py
+
+
+
+# *-1 打印
+# 直接打印
+print("Hello my Bug1")
+
+# *-2 变量
+# 使用变量，就目前而言，应使用小写的Python变量名。
+# 在变量名中使用大写字母虽然不会导致错误，但避免使用大写字母是个不错的习惯。
+message = "Hello my Bug2"
+print(message)
+
+# *-3 字符串 在Python中，用括号引起的都是字符串，其中的引号可以是单引号，也可以是双引号。
+name = "ada lovelace"
+print(name.title())
+
+
+# *-
+# *-
+# *-
+
