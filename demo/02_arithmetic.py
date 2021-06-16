3d087310540f1896823872c39f9d7e29517ebcf9
Q Lee <qliangw@outlook.com>
2021-06-16 10:16:26 +0800

    Update str的使用

--- demo/02_arithmetic.py
+++ demo/02_arithmetic.py
@@ -15,6 +15,10 @@ print("---------------------------------")
 # 括号
 print("2 + 3 * 4 =", 2+3*4)
 print("(2 + 3) * 4 =", (2+3)*4)
-
+print("---------------------------------")
+# str
+age = 18
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
 
 
