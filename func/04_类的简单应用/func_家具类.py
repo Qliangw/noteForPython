f1ad11dd6c9563a3aaf9ae633b4a0750a21cabfd
Jason_Smith <qili00001@gmail.com>
2021-06-22 18:23:07 +0800

    清理冗余

--- func/04_类的简单应用/func_家具类.py
+++ func/04_类的简单应用/func_家具类.py
@@ -6,8 +6,3 @@ class HouseItem:
 
     def __str__(self):
         return "[%s]占地%.2f平方" % (self.name, self.area)
-
-
-bed = HouseItem("席梦思", 4)
-chest = HouseItem("衣柜", 2)
-table = HouseItem("餐桌", 1.5)
